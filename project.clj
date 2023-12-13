6c6716d54036fb4e4995dfff526f13ae5566f870
Alexander Olshansky <sander2411ukr@gmail.com>
2023-12-13 13:04:16 +0100

    add deps

--- project.clj
+++ project.clj
@@ -3,7 +3,11 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [http-kit/http-kit "2.7.0"]
+                 [cheshire "5.12.0"]
+                 [compojure "1.7.0"]
+                 [ring/ring-defaults "0.4.0"]]
   :main ^:skip-aot b-chat.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
